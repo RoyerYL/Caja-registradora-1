1ce50a466662eed3f132e31d596bea709442fb5f
RoyerYampasi <royeryl03@gmail.com>
2023-12-12 22:45:43 -0300

    agregando articulos al ticket

--- client/src/Utils/index.js
+++ client/src/Utils/index.js
@@ -7,18 +7,48 @@ pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 async function createPDF() {
 
-    const id = 7
+    const id = 17
+    let compras = []
+    let fecha = ""
+    let ticketId = 0
+    let costo = 0
+    let items = {}
+    let nombreArt = ""
     if (!id) {
         throw new Error("faltan datos")
     }
-    const { data } = await axios(`http://localhost:3001/tienda/ticket/${id}`)
-    if (!data) {
-        throw new Error
+    await axios(`http://localhost:3001/tienda/ticket/${id}`).then(({ data }) => {
+        fecha = data.fecha
+        ticketId = String(data.id).padStart(12, '0');
+        costo = data.valorTotal
+    })
+
+    const item = async (codBarras) => {
+
     }
-    const ticketId = String(data.id).padStart(12, '0');
+    await axios(`http://localhost:3001/tienda/compra/${id}`).then(async ({ data }) => {
+
+        items = await Promise.all(data.map(async (compra) => {
+            const { data } = await axios(`http://localhost:3001/tienda/articulo/${compra.ArticuloId}`)
+            const total=compra.cantidad * data.precioVenta
+            // return { text: `${data.name} \t ${compra.cantidad} \t 10$`, fontSize: 9, margin: [0, 0, 0, 5] }
+            return {
+                columns: [
+                    { text: data.name, fontSize: 7},
+                    { text: compra.cantidad, fontSize: 7 },
+                    { text: total, fontSize: 9 },
+                ],
+                margin: [0, 0, 0, 5],
+            };
+        }))
+
+    })
+
+
     // Define el tamaño de la página en puntos (aproximadamente 80 mm de ancho)
     const pageSize = { width: 226.8, height: 1700 };
-    const costo = data.valorTotal
+
+
     // Define la estructura del documento PDF
     const documentDefinition = {
         pageSize,  // Establece el tamaño de la página
@@ -32,10 +62,11 @@ async function createPDF() {
             { text: 'iva responsable inscripto:', fontSize: 8, alignment: 'left', margin: [0, 0, 0, 3] },
             { text: '------------------------------------------------------------------', fontSize: 8, alignment: 'left', margin: [0, 0, 0, 3] },
             { text: 'Recibo de Venta', fontSize: 16, alignment: 'center', margin: [0, 0, 0, 10] },
-            { text: 'Fecha: 24 de noviembre de 2023', fontSize: 12, margin: [0, 0, 0, 10] },
+            { text: fecha, fontSize: 12, margin: [0, 0, 0, 10] },
             { text: '------------------------------------------------------------------', fontSize: 8, alignment: 'left', margin: [0, 0, 0, 3] },
-            { text: 'Artículo 1    $10.00', fontSize: 9, margin: [0, 0, 0, 5] },
-            { text: 'Artículo 2    $20.00', fontSize: 9, margin: [0, 0, 0, 5] },
+            items,
+            // { text: 'Artículo 1    $10.00', fontSize: 9, margin: [0, 0, 0, 5] },
+            // { text: 'Artículo 2    $20.00', fontSize: 9, margin: [0, 0, 0, 5] },
             { text: '------------------------------------------------------------------', fontSize: 8, alignment: 'left', margin: [0, 0, 0, 3] },
             { text: `Subtotal:$ ${costo}`, fontSize: 9, margin: [0, 0, 0, 5] },
             { text: `Rec:$`, fontSize: 7, margin: [0, 0, 0, 0] },
