ac842ce1705afa002b6b0f05b9be420134554971
RoyerFrba <royeryl03@gmail.com>
2024-01-02 09:08:26 -0300

    alta provedor

--- client/src/components/InrgesoMercaderia/IngresoMercaderia.jsx
+++ client/src/components/InrgesoMercaderia/IngresoMercaderia.jsx
@@ -5,7 +5,9 @@ import style from './IngresoMercaderia.module.css'
 import { Link } from 'react-router-dom'
 export default function IngresoMercaderia() {
     const [allProductos, setAllProductos] = useState([])
+    const [productosSeleccionados, setSeleccionados] = useState([])
     const [allProductosAux, setAllProductosAux] = useState([])
+    const [buscador,setBuscador]=useState("")
     useEffect(() => {
         axios(`http://localhost:3001/tienda/articulo`).then(({ data }) => {
             setAllProductos(data)
@@ -28,14 +30,25 @@ export default function IngresoMercaderia() {
         const newList = [...allProductosAux].sort((a, b) => a.stock - b.stock)
         setAllProductos(newList)
     }
+    const filter=(e)=>{
+        const value=e.target.value
+        setBuscador(value)
+
+        
+        const newList=[...allProductos].filter((prod)=>
+        {
+            return prod.id===value || prod.name===value
+        })
+        console.log(newList);
+    }
 
     return (
         <div>
             <div>
-                <div>
-                    <input type="text" />
-                    <button>buscar</button>
-                </div>
+                <input type="text" name='buscador' value={buscador} onChange={filter} />
+                <button>buscar</button>
+            </div>
+            <div className='flex-1'>
                 <div>
                     <div className={style.listaArticulos}>
                         <div className={style.listArticuloEncontrados}>
@@ -49,6 +62,35 @@ export default function IngresoMercaderia() {
                             </div>
                             {
                                 allProductos.map((prod, index) => (
+                                        <div className={style.articulos} key={prod.id}>
+                                            <p className={style.codBarras}>{prod.id}</p>
+                                            <p className={style.nombre}>{prod.name}</p>
+                                            <p>{prod.stock}</p>
+                                            <p className={style.precio}>${prod.precioVenta}</p>
+                                        </div>
+                                ))
+                            }
+
+
+                        </div>
+                    </div>
+                </div>
+                <div className='flex-2'>
+                    <button>{`->`}</button>
+                    <button>Todo</button>
+                </div>
+                <div>
+                    <div className={style.listaArticulos}>
+                        <div className={style.listArticuloEncontrados}>
+
+                            <div className='flex-1'>
+                                <p >codigo de Barras</p>
+                                <p >Nombre</p>
+                                <p >Cantidad</p>
+                                <p >precio</p>
+                            </div>
+                            {
+                                productosSeleccionados.map((prod, index) => (
                                     <Link key={prod.id} to={`/detail/${prod.id}`}>
                                         <div className={style.articulos} key={prod.id}>
                                             <p className={style.codBarras}>{prod.id}</p>
@@ -60,6 +102,39 @@ export default function IngresoMercaderia() {
                                 ))
                             }
 
+
+                        </div>
+                    </div>
+                    <div>
+
+                        <div>
+                            <label htmlFor="">Comentarios</label>
+                            <input type="text" />
+
+                        </div>
+                        <div>
+                            <label htmlFor="">Sub total</label>
+                            <input type="text" />
+
+                        </div>
+                        <div>
+                            <label htmlFor="">Descuento</label>
+                            <input type="text" />
+
+                        </div>
+                        <div>
+                            <label htmlFor="">Total iva</label>
+                            <input type="text" />
+
+                        </div>
+                        <div>
+                            <label htmlFor="">Percepciones</label>
+                            <input type="text" />
+
+                        </div>
+                        <div>
+                            <label htmlFor="">Total</label>
+                            <input type="text" />
 
                         </div>
                     </div>
