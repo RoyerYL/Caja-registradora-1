36a1ce9f263e996074b452598badf2890ed977ec
RoyerFrba <royeryl03@gmail.com>
2024-03-22 13:32:57 -0300

    bd

--- client/src/index.js
+++ client/src/index.js
@@ -3,21 +3,21 @@ const { dataBase, Articulo, Categoria, TicketData } = require('../server/src/DB_
 
 const PORT = 3001;
 
-dataBase.sync({ alter: false }).then(() => {
+dataBase.sync({ force:false }).then(() => {
   server.listen(PORT, () => {
     console.log('Server raised in port: ' + PORT);
 
     // Realizar la operación de alteración solo para la tabla Articulos después de iniciar el servidor
-    Articulo.sync({ alter: true }).then(() => {
-      console.log('Operación de alteración completada para la tabla Articulos.');
-    }).catch((error) => {
-      console.error('Error al realizar la operación de alteración para la tabla Articulos:', error);
-    });
+    // Articulo.sync({ alter: true }).then(() => {
+    //   console.log('Operación de alteración completada para la tabla Articulos.');
+    // }).catch((error) => {
+    //   console.error('Error al realizar la operación de alteración para la tabla Articulos:', error);
+    // });
     // Realizar la operación de alteración solo para la tabla Articulos después de iniciar el servidor
-    TicketData.sync({ alter: true }).then(() => {
-      console.log('Operación de alteración completada para la tabla TicketData.');
-    }).catch((error) => {
-      console.error('Error al realizar la operación de alteración para la tabla Articulos:', error);
-    });
+    // TicketData.sync({ alter: true }).then(() => {
+    //   console.log('Operación de alteración completada para la tabla TicketData.');
+    // }).catch((error) => {
+    //   console.error('Error al realizar la operación de alteración para la tabla Articulos:', error);
+    // });
   });
 });
